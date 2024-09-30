2cc0a7fb31912149f3333884d7aabfb2e6573102
Mariojunior2 <mario.junior2@alunos.sc.senac.br>
2024-09-30 16:20:43 -0300

    A
    Co-authored-by: Vinícius Gabriel Inacio <Inacio727@users.noreply.github.com>

--- curiosidades.php
+++ curiosidades.php
@@ -1,6 +1,45 @@
 <div id="contentCuriosidades">
     <h1>Curiosidades</h1>
-    <ul class="curio">
+    <ul class="curio" style="list-style: none;">
+    <li><a style="font-size: 50px;">1. Algoritmos Famosos</a></li>
+    <div class="row d-flex" style="margin-left: 200px;">
+    <img src="https://3.bp.blogspot.com/-cpKSVft4OIw/Wdj7jQKphBI/AAAAAAAAAAg/86GmfhwKpGIXxuKY0xGfI7DPD3NcS6ijwCLcBGAs/w1200-h630-p-k-no-nu/algortimo.jpg" style="width: 500px;" class="col-6"> 
+    <p class="col-6" style="margin-top: 3%;">Algoritmo de Dijkstra: Criado por Edsger Dijkstra em 1956, facilitou rotas em redes de computadores. Quick Sort: Tony Hoare criou este algoritmo de ordenação em 1960.</p>
+    </div>
+</ul>
 
-    </ul>
+<ul style="list-style: none;">
+    <li><a style="font-size: 50px;">2. Primeira Programadora</a></li>
+    <div class="row d-flex" style="margin-left: 200px;">
+    <img src="https://s2-techtudo.glbimg.com/TM4pgt_uZDMaADiSbDhOB4X_160=/0x0:1200x675/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/r/j/X00R0WS22lOFjrfvQYgg/5f15f3509f331.jpeg" style="width: 500px;" class="col-6"> 
+    <p class="col-6" style="margin-top: 5%;">Ada Lovelace, no século 19, escreveu o primeiro algoritmo para a máquina de Charles Babbage, sendo considerada a primeira programadora.</p>
+    </div>
+</ul>
+
+<ul style="list-style: none;">
+    <li><a style="font-size: 50px;">3. Linguagem de Programação mais Antiga</a></li>
+    <div class="row d-flex" style="margin-left: 200px;">
+    <img src="https://www.ufsm.br/app/uploads/sites/791/2021/05/Fortran4.jpeg" style="width: 500px;" class="col-6"> 
+    <p class="col-6" style="margin-top: 7%;">Fortran, criada em 1957, é considerada a linguagem de programação mais antiga ainda em uso.</p>
+    </div>
+</ul>
+<ul style="list-style: none;">
+    <li><a style="font-size: 50px;">4. O Bug de Computador</a></li>
+    <div class="row d-flex" style="margin-left: 200px;">
+    <img src="https://3.bp.blogspot.com/-S385EOFa8-o/WTliEKzQwTI/AAAAAAAAF4M/9Gaxmnj-GPo6CUShrEm1v5-AroPyr_2nQCLcB/s1600/bug_mariposa1.jpg" style="width: 500px;" class="col-6"> 
+    <p class="col-6" style="margin-top: 5%;">Em 1947, Grace Hopper documentou o primeiro "bug" de computador: uma mariposa presa em um relé de um computador.</p>
+    </div>
+</ul>
+
+<ul style="list-style: none; padding: 5%;">
+    <li><a style="font-size: 50px;">5. O Primeiro Vírus de Computador</a></li>
+    <div class="row d-flex" style="margin-left: 200px;">
+    <img src="https://www.dynadot.com/community/image?image_id=772" style="width: 500px;" class="col-6"> 
+    <p class="col-6" style="margin-top: 5%;">Em 1986, o vírus Brain foi criado no Paquistão, sendo o primeiro vírus de computador para PC.</p>
+    </div>
+</ul>
+    
+   
+    
+    </div>
 </div>
